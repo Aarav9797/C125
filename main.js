511bfcbd03593e63f4180732f11eceb3ea8ff506
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-05-11 17:37:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
+var song="";
 function setup(){
     canvas=createCanvas(600,500)
     canvas.center()
+    video=createCapture(VIDEO)
+    video.hide()
 }
-function draw(){}
-function preload(){}
+function draw(){
+    image(video,0,0,600,500)
+}
+function preload(){
+    song=loadSound("music.mp3");
+}
+function songplay(){
+    song.rate(1)
+    song.setVolume(0.2)
+    song.play()
+}
+function songstop(){
+    song.stop()
+}
