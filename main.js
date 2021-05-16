9f84621222eec5d44305499393e788ac2d9dae4f
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-05-16 13:26:39 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 var song="";
+var left_wrist_x=0;
+var left_wrist_y=0;
+var right_wrist_x=0;
+var right_wrist_y=0;
 function setup(){
     canvas=createCanvas(600,500)
     canvas.center()
     video=createCapture(VIDEO)
     video.hide()
+    console.log(ml5.version)
+ poseNet=ml5.poseNet(video,modelloaded)
+ poseNet.on("pose",gotposes);
 }
 function draw(){
     image(video,0,0,600,500)
@@ -18,4 +25,17 @@ function songplay(){
 }
 function songstop(){
     song.stop()
+}
+function modelloaded(){
+    console.log("your model has been loaded")
+}
+function gotposes(results){
+if(results.length>0){
+    console.log(results);
+   right_wrist_x=results[0].pose.rightWrist.x;
+   right_wrist_y=results[0].pose.rightWrist.y;
+   left_wrist_x=results[0].pose.leftWrist.x;
+   left_wrist_y=results[0].pose.leftWrist.y;
+   console.log(right_wrist_x,right_wrist_y,left_wrist_x,left_wrist_y);
+}
 }
