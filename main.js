1ff20201fae7fb0704a061549952db9e5bc2dee9
Aarav9797 <71883558+Aarav9797@users.noreply.github.com>
2021-05-18 17:45:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ var left_wrist_x=0;
 var left_wrist_y=0;
 var right_wrist_x=0;
 var right_wrist_y=0;
+var score_left_wrist=0;
+var score_right_wrist=0;
 function setup(){
     canvas=createCanvas(600,500)
     canvas.center()
@@ -14,6 +16,15 @@ function setup(){
 }
 function draw(){
     image(video,0,0,600,500)
+    if(score_left_wrist>0.2){
+        fill(255,0,0) 
+        circle(left_wrist_x,left_wrist_y,20)
+        volume_value=Number(left_wrist_y);
+        volume_value=floor(volume_value);
+        volume_value=volume_value/500;
+        document.getElementById("volume2").innerHTML=volume_value;
+        song.setVolume(volume_value);
+    }
 }
 function preload(){
     song=loadSound("music.mp3");
@@ -36,6 +47,7 @@ if(results.length>0){
    right_wrist_y=results[0].pose.rightWrist.y;
    left_wrist_x=results[0].pose.leftWrist.x;
    left_wrist_y=results[0].pose.leftWrist.y;
-   console.log(right_wrist_x,right_wrist_y,left_wrist_x,left_wrist_y);
+   score_left_wrist=results[0].pose.keypoints[9].score;
+   score_right_wrist=results[0].pose.keypoints[10].score;
 }
 }
